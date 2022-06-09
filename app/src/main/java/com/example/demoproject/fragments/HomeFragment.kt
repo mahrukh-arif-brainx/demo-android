6dd1e6744e5a33b94704e09101dae22416aa5db0
mahrukh-arif-brainx <mahrukh.arif@brainxtech.com>
2022-06-09 22:04:02 +0500

    view-pager

---
+++ app/src/main/java/com/example/demoproject/fragments/HomeFragment.kt
@@ -0,0 +1,57 @@
+package com.example.demoproject.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.demoproject.HomeActivityViewModel
+import com.example.demoproject.R
+import com.example.demoproject.TrainingActivity
+import com.example.demoproject.models.AppSharedPreference
+import kotlinx.android.synthetic.main.activity_home.*
+
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        name.text= AppSharedPreference.getAppSharedPreferences(requireContext())?.getStringValue("name")
+
+
+        val viewModel = ViewModelProvider(requireActivity()).get(HomeActivityViewModel::class.java)
+        viewModel.getAuthorAndQuote(requireContext())
+        viewModel.authorAndQuote.observe(viewLifecycleOwner, Observer {
+            Quotation_text.text=it.text
+            Quotation_author.text=it.author
+        })
+        viewModel.msg.observe(viewLifecycleOwner, Observer {
+            Quotation.visibility= View.GONE
+
+        })
+        val intent= Intent(activity, TrainingActivity::class.java)
+
+        training_card_view.setOnClickListener{
+            startActivity(intent)
+        }
+    }
+
+
+}
