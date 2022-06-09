74c68e4ea2142e1249e4d0b9670bc954cb8c376d
mahrukh-arif-brainx <mahrukh.arif@brainxtech.com>
2022-06-09 22:21:59 +0500

    adapter

---
+++ app/src/main/java/com/example/demoproject/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.demoproject.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.demoproject.fragments.HomeFragment
+import com.example.demoproject.fragments.NotificationFragment
+import com.example.demoproject.fragments.SettingsFragment
+
+private const val NUM_TABS = 3
+
+class PagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle):FragmentStateAdapter(fragmentManager,lifecycle) {
+    override fun getItemCount(): Int {
+         return NUM_TABS
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when(position){
+            1->return HomeFragment()
+            2->return NotificationFragment()
+        }
+       return SettingsFragment()
+    }
+
+}
